f225182707402839a456677bbc20a2abcb23122b
Turing <kevin831130810620@gmail.com>
2015-10-09 03:23:57 +0800

    fix wrong commit

--- application/controllers/RoomEngine.php
+++ application/controllers/RoomEngine.php
@@ -27,12 +27,17 @@ class RoomEngine extends CI_Controller
 		$this->view_header();
 		// sidebar
 		$data['active'] = 0;
-		$this->load->view('roomengine/sidebar', $data);
+		$this->load->view('contract/sidebar', $data);
 		// body table
+<<<<<<< HEAD
 		$data['dormlist'] = $this->Mutility->get_dorm_list();
 <<<<<<< HEAD
 <<<<<<< HEAD
 		$this->load->view('roomengine/index/search_table', $data);
+=======
+		// $data['dormlist'] = $this->Mutility->get_dorm_list();
+		// $this->load->view('contract/index/search_table', $data);
+>>>>>>> parent of 2803106... wrong commit
 		// $data['saleslist'] = $this->Mutility->get_user_list();
 		// $this->load->view('contract/index/viewModel',$data);
 		// $this->load->view('contract/index/checkout');
--- views/roomengine/sidebar.php
+++ views/roomengine/sidebar.php
@@ -2,7 +2,7 @@
 $sidebardata = array(array(	'name'=>'',
 							'style'=>'',
 							'data'=>array(
-								array('icon'=>'','url'=>web_url("/roomEngine/index"),'text'=>'房間搜尋'),
+								array('icon'=>'','url'=>web_url("/roomEngine/index"),'text'=>'房間搜尋')	
 								array('icon'=>'','url'=>web_url("#"),'text'=>'時間軸')
 							)
 						)
